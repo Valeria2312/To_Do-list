88f3c0997f437b203f36a4f5edf05a5c1d88d0b6
Valeria Koroleva <52246450+Valeria2312@users.noreply.github.com>
2022-05-28 00:10:00 +0300

    Add files via upload

---
+++ src/views/components/InputPlus/index.tsx
@@ -0,0 +1,39 @@
+import React, {useCallback, useState} from "react";
+import styles from "./index.module.scss";
+
+
+interface InputPlusProps {
+    onAdd: (title: string) => void;
+}
+
+
+export const InputPlus: React.FC <InputPlusProps> = ({ onAdd })  => {
+    const [inputValue, setInputValue] = useState('');
+    const addTask = useCallback( () => {
+        onAdd(inputValue);
+        setInputValue('')
+        }, [inputValue])
+    return (
+        <div className = {styles.inputPlus}>
+            <input
+            type = "text"
+            className={styles.inputPlusValue}
+            value={inputValue}
+            onChange={(event) => {
+                setInputValue(event.target.value);
+            }}
+            onKeyDown={(event) => {
+                if(event.key === 'Enter') {
+                    addTask();
+                }
+            }}
+            placeholder = "Type here..."
+            />
+            <button onClick={addTask}
+            className={styles.inputPlusButton}
+            aria-label="Add"
+            />
+
+        </div>
+    )
+}
